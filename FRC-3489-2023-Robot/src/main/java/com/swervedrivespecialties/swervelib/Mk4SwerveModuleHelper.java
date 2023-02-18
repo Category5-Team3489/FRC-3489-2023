6ff9b9561cf50df9de44dc386ac91b6cc9a755d8
3489ProgrammingFleet <98435354+3489ProgrammingFleet@users.noreply.github.com>
2023-02-18 13:56:21 -0500

    Change swerve steering P constant

--- FRC-3489-2023-Robot/src/main/java/com/swervedrivespecialties/swervelib/Mk4SwerveModuleHelper.java
+++ FRC-3489-2023-Robot/src/main/java/com/swervedrivespecialties/swervelib/Mk4SwerveModuleHelper.java
@@ -20,7 +20,7 @@ public final class Mk4SwerveModuleHelper {
     private static SteerControllerFactory<?, Falcon500SteerConfiguration<CanCoderAbsoluteConfiguration>> getFalcon500SteerFactory(Mk4ModuleConfiguration configuration) {
         return new Falcon500SteerControllerFactoryBuilder()
                 .withVoltageCompensation(configuration.getNominalVoltage())
-                .withPidConstants(0.2, 0.0, 0.1)
+                .withPidConstants(0.2 * 1.3, 0.0, 0.1)
                 .withCurrentLimit(configuration.getSteerCurrentLimit())
                 .build(new CanCoderFactoryBuilder()
                         .withReadingUpdatePeriod(100)
