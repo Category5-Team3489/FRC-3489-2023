4a564e6fd949ba11bc0749f447a1acd1d37f5131
blake <blake.deason@gmail.com>
2023-03-06 12:27:01 -0500

    Revised color sensor reinitialize

--- FRC-3489-2023-Comp/src/main/java/frc/robot/subsystems/ColorSensor.java
+++ FRC-3489-2023-Comp/src/main/java/frc/robot/subsystems/ColorSensor.java
@@ -10,15 +10,16 @@ import edu.wpi.first.wpilibj.util.Color;
 import frc.robot.enums.GamePiece;
 import frc.robot.configs.colorsensor.ColorAndProximityConfig;
 import frc.robot.shuffleboard.Cat5ShuffleboardTab;
+import frc.robot.subsystems.Leds.LedState;
 
 public class ColorSensor extends Cat5Subsystem<ColorSensor> {
-    //#region Singleton
+    // #region Singleton
     private static ColorSensor instance = new ColorSensor();
 
     public static ColorSensor get() {
         return instance;
     }
-    //#endregion
+    // #endregion
 
     // Configs
     public final ColorAndProximityConfig colorAndProximityConfig = new ColorAndProximityConfig();
@@ -37,17 +38,17 @@ public class ColorSensor extends Cat5Subsystem<ColorSensor> {
 
         // proximity: 0 to 2047
         // https://docs.wpilib.org/en/stable/docs/yearly-overview/known-issues.html#onboard-i2c-causing-system-lockups
-    
-        //#region Shuffleboard
+
+        // #region Shuffleboard
         var layout = getLayout(Cat5ShuffleboardTab.Main, BuiltInLayouts.kList)
-            .withSize(2, 3);
+                .withSize(2, 3);
 
         layout.addDouble("Red", () -> color.red);
         layout.addDouble("Green", () -> color.green);
         layout.addDouble("Blue", () -> color.blue);
         layout.addString("Proximity", () -> Integer.toString(proximity));
         layout.addString("Detected Game Piece", () -> detectedGamePiece.toString());
-        //#endregion
+        // #endregion
     }
 
     @Override
@@ -55,35 +56,27 @@ public class ColorSensor extends Cat5Subsystem<ColorSensor> {
         color = colorSensor.getColor();
         proximity = colorSensor.getProximity();
 
-        if (!colorSensor.isConnected()) {
-            reconnectTimer.start();
-            System.out.println("Color sensor is not connected!!!");
-            
-            if (reconnectTimer.advanceIfElapsed(2.0)) {
-                System.out.println("Color sensor has been disconnected for more than 2 seconds, reconnecting...");
-                colorSensor = new ColorSensorV3(I2C.Port.kMXP);
+        if (isColorSensorWorking()) {
+            if (proximity < colorAndProximityConfig.getDetectionProximity.get()) {
+                detectedGamePiece = GamePiece.Unknown;
+            } else {
+                Translation3d current = new Translation3d(color.red, color.green, color.blue);
+                Color coneColor = colorAndProximityConfig.getConeColor.get();
+                Color cubeColor = colorAndProximityConfig.getCubeColor.get();
+                double cone = current.getDistance(new Translation3d(coneColor.red, coneColor.green, coneColor.blue));
+                double cube = current.getDistance(new Translation3d(cubeColor.red, cubeColor.green, cubeColor.blue));
+                if (cone <= cube) {
+                    detectedGamePiece = GamePiece.Cone;
+                } else {
+                    detectedGamePiece = GamePiece.Cube;
+                }
             }
-        }
-
-        if (proximity < colorAndProximityConfig.getDetectionProximity.get()) {
-            detectedGamePiece = GamePiece.Unknown;
-        }
-        else {
-            Translation3d current = new Translation3d(color.red, color.green, color.blue);
-            Color coneColor = colorAndProximityConfig.getConeColor.get();
-            Color cubeColor = colorAndProximityConfig.getCubeColor.get();
-            double cone = current.getDistance(new Translation3d(coneColor.red, coneColor.green, coneColor.blue));
-            double cube = current.getDistance(new Translation3d(cubeColor.red, cubeColor.green, cubeColor.blue));
-            if (cone <= cube) {
-                detectedGamePiece = GamePiece.Cone;
-            }
-            else {
-                detectedGamePiece = GamePiece.Cube; 
-            }
+        } else {
+            Leds.get().setLeds(LedState.Red);
         }
     }
 
-    //#region Public
+    // #region Public
     public Color getColor() {
         return color;
     }
@@ -95,5 +88,25 @@ public class ColorSensor extends Cat5Subsystem<ColorSensor> {
     public GamePiece getDetectedGamePiece() {
         return detectedGamePiece;
     }
-    //#endregion
+
+    public boolean isColorSensorWorking() {
+        if (!colorSensor.isConnected()) {
+            reconnectTimer.start();
+            System.out.println("Color sensor is not connected!!!");
+
+            if (reconnectTimer.advanceIfElapsed(2.0)) {
+                System.out.println("Color sensor has been disconnected for more than 2 seconds, reconnecting...");
+                colorSensor = new ColorSensorV3(I2C.Port.kMXP);
+                return true;
+            }
+        }
+
+        // in connected but not working situation, reconstruct and return.
+        if ((proximity == 0 && color.blue == 0 && color.red == 0)) {
+            colorSensor = new ColorSensorV3(I2C.Port.kMXP);
+            return true;
+        }
+        return true;
+    }
+    // #endregion
 }
--- FRC-3489-2023-Comp/src/main/java/frc/robot/subsystems/Leds.java
+++ FRC-3489-2023-Comp/src/main/java/frc/robot/subsystems/Leds.java
@@ -19,13 +19,13 @@ import frc.robot.Constants.LedConstants;
 import frc.robot.shuffleboard.Cat5ShuffleboardTab;
 
 public class Leds extends Cat5Subsystem<Leds> {
-    //#region Singleton
+    // #region Singleton
     private static Leds instance = new Leds();
-    
+
     public static Leds get() {
         return instance;
     }
-    //#endregion
+    // #endregion
 
     private boolean haveTeleopLedsFlashedThisEnable = false;
 
@@ -34,45 +34,55 @@ public class Leds extends Cat5Subsystem<Leds> {
 
     public LedState ledState = LedState.Off;
 
-    // TODO Limit led setting bandwidth, compare led buffer with applied and unapplied on update then only set if they are different
+    // TODO Limit led setting bandwidth, compare led buffer with applied and
+    // unapplied on update then only set if they are different
 
-   private Leds() {
-    super((i) -> instance = i);
+    private Leds() {
+        super((i) -> instance = i);
         // led.setLength(buffer.getLength());
         // led.setData(buffer);
         // led.start();
 
         RobotContainer.get().man.axisLessThan(3, Constants.LedConstants.CubeLEDManipulator)
-        .onTrue(Commands.runOnce(() -> {
-            setLeds(LedState.NeedCube);
-        }))
-        .onFalse(Commands.runOnce(() -> {
-            setLeds(LedState.Off);
-        }));
+                .onTrue(Commands.runOnce(() -> {
+                    setLeds(LedState.NeedCube);
+                }))
+                .onFalse(Commands.runOnce(() -> {
+                    setLeds(LedState.Off);
+                }));
 
         RobotContainer.get().man.axisGreaterThan(3, Constants.LedConstants.ConeLEDManipulator)
-        .onTrue(Commands.runOnce(() -> {
-            setLeds(LedState.NeedCone);
-        }))
-        .onFalse(Commands.runOnce(() -> {
-            setLeds(LedState.Off);
-        }));
+                .onTrue(Commands.runOnce(() -> {
+                    setLeds(LedState.NeedCone);
+                }))
+                .onFalse(Commands.runOnce(() -> {
+                    setLeds(LedState.Off);
+                }));
 
         if (RobotState.isDisabled() && getCurrentAlliance() == "Blue")
             setLeds(LedState.DisabledPatternBlue);
         else if (RobotState.isDisabled() && getCurrentAlliance() == "Red")
             setLeds(LedState.DisabledPatternRed);
-        else setLeds(LedState.Off);
-        
+        else
+            setLeds(LedState.Off);
 
         var layout = getLayout(Cat5ShuffleboardTab.Main, BuiltInLayouts.kList)
-            .withSize(2, 3);
+                .withSize(2, 3);
 
         layout.addString("LED", () -> ledState.toString());
 
         var mainLayout = getLayout(Cat5ShuffleboardTab.Main, BuiltInLayouts.kList)
-        .withSize(2, 4);
+                .withSize(2, 4);
         mainLayout.addString("LED State", () -> ledState.toString());
+    }
+
+    private String getCurrentAlliance() {
+        if (DriverStation.getAlliance() == Alliance.Blue)
+            return "Blue";
+        else if (DriverStation.getAlliance() == Alliance.Red)
+            return "Red";
+        else
+            return "Invalid";
     }
 
     public enum LedState {
@@ -93,36 +103,36 @@ public class Leds extends Cat5Subsystem<Leds> {
         this.ledState = ledState;
 
         switch (ledState) {
-        case Off: //Off
-            stopLeds();
-        break;
-        case TeleopBlink: //Lawn Green
-            setSolidColor(0.71);
-        break;
-        case NeedCone: //Yellow
-            setSolidColor(0.69);
-        break;
-        case NeedCube: //Blue Violet 
-            setSolidColor(0.89);
-        break;
-        case PlaceCone: //Yellow
-            setSolidColor(0.69);
-        break;
-        case PlaceCube: //Violet 
-            setSolidColor(0.91);
-        break;
-        case DarkRed:
-            setSolidColor(0.59);
-        break;
-        case Red:
-            setSolidColor(0.61);
-        break;
-        case DisabledPatternBlue:
-            setSolidColor(0.7);
-        break;
-        case DisabledPatternRed:
-            setSolidColor(0.27);
-            break;
+            case Off: // Off
+                stopLeds();
+                break;
+            case TeleopBlink: // Lawn Green
+                setSolidColor(0.71);
+                break;
+            case NeedCone: // Yellow
+                setSolidColor(0.69);
+                break;
+            case NeedCube: // Blue Violet
+                setSolidColor(0.89);
+                break;
+            case PlaceCone: // Yellow
+                setSolidColor(0.69);
+                break;
+            case PlaceCube: // Violet
+                setSolidColor(0.91);
+                break;
+            case DarkRed:
+                setSolidColor(0.59);
+                break;
+            case Red:
+                setSolidColor(0.61);
+                break;
+            case DisabledPatternBlue:
+                setSolidColor(0.7);
+                break;
+            case DisabledPatternRed:
+                setSolidColor(0.27);
+                break;
             default:
                 break;
         }
@@ -139,19 +149,19 @@ public class Leds extends Cat5Subsystem<Leds> {
     }
 
     // public void setSolidColor(LedColor color) {
-    //     for (var i = 0; i < buffer.getLength(); i++) {
-    //         color.apply(i, buffer);
-    //     }
-    //     led.setData(buffer);
-    //     colorEntry.setString(color.toString());
+    // for (var i = 0; i < buffer.getLength(); i++) {
+    // color.apply(i, buffer);
+    // }
+    // led.setData(buffer);
+    // colorEntry.setString(color.toString());
     // }
 
     // public void stopLeds() {
-    //     setSolidColor(LedColor.Off);
+    // setSolidColor(LedColor.Off);
     // }
-    
+
     @Override
-    public void periodic() {     
+    public void periodic() {
         tryFlashTeleopLeds();
     }
 
@@ -165,13 +175,13 @@ public class Leds extends Cat5Subsystem<Leds> {
             return;
         }
         Commands.runOnce(() -> setLeds(LedState.TeleopBlink), this)
-            .withTimeout(1)
-            .withInterruptBehavior(InterruptionBehavior.kCancelIncoming)
-            .schedule();
-            
+                .withTimeout(1)
+                .withInterruptBehavior(InterruptionBehavior.kCancelIncoming)
+                .schedule();
+
         // Command command = new FlashLeds(this, LedColor.White, 10, 0.1, 0.1)
-        //     .withInterruptBehavior(InterruptionBehavior.kCancelIncoming);
-        
+        // .withInterruptBehavior(InterruptionBehavior.kCancelIncoming);
+
         haveTeleopLedsFlashedThisEnable = true;
     }
 
@@ -183,19 +193,12 @@ public class Leds extends Cat5Subsystem<Leds> {
             stopLeds();
         };
 
-        InterruptionBehavior interruptBehavior = isInterruptible ?
-            InterruptionBehavior.kCancelSelf : InterruptionBehavior.kCancelIncoming;
+        InterruptionBehavior interruptBehavior = isInterruptible ? InterruptionBehavior.kCancelSelf
+                : InterruptionBehavior.kCancelIncoming;
 
         return Commands.startEnd(start, end, this)
-            .withTimeout(seconds)
-            .withInterruptBehavior(interruptBehavior);
+                .withTimeout(seconds)
+                .withInterruptBehavior(interruptBehavior);
     }
 
-    private String getCurrentAlliance(){
-        if (DriverStation.getAlliance() == Alliance.Blue)
-        return "Blue";
-        else if (DriverStation.getAlliance() == Alliance.Red)
-        return "Red";
-        else return "Invalid";
-    }
- }
+}
