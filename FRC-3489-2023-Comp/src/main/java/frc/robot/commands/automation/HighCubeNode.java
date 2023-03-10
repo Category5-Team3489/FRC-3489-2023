c0cb20da55f2b5ad111843b5f6fc358d3bd73eaf
3489ProgrammingFleet <98435354+3489ProgrammingFleet@users.noreply.github.com>
2023-03-10 18:59:08 -0500

    Update HighCubeNode.java

--- FRC-3489-2023-Comp/src/main/java/frc/robot/commands/automation/HighCubeNode.java
+++ FRC-3489-2023-Comp/src/main/java/frc/robot/commands/automation/HighCubeNode.java
@@ -1,35 +1,22 @@
 package frc.robot.commands.automation;
 
-import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants.LimelightConstants;
+import frc.robot.commands.DriveToRelativePose;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Limelight;
 
 public class HighCubeNode extends CommandBase {
-    private PIDController strafeController = new PIDController(0.18, 0, 0);
-    private PIDController distanceController = new PIDController(0.18, 0, 0);
-
-    private double xMetersPerSecond = 0;
-    private double yMetersPerSecond = 0;
+    private final DriveToRelativePose command = null;
     
     public HighCubeNode() {
-        strafeController.setTolerance(1.5);
-        distanceController.setTolerance(1.5);
+
     }
 
     @Override
     public void initialize() {
         Limelight.get().setDesiredPipeline(LimelightConstants.FiducialPipeline);
-
-        Drivetrain.get().driveCommand.setTargetAngle(Rotation2d.fromDegrees(180));
-        Drivetrain.get().driveCommand.setAutomationXSupplier(() -> xMetersPerSecond);
-        Drivetrain.get().driveCommand.setAutomationYSupplier(() -> yMetersPerSecond);
-        Drivetrain.get().driveCommand.setAutomationSpeedLimiterSupplier(() -> 0.5);
-
-        System.out.println("High cube node init");
     }
 
     @Override
@@ -43,35 +30,21 @@ public class HighCubeNode extends CommandBase {
             return;
         }
 
-        double targetX = Limelight.get().getTargetX();
-        if (!Double.isNaN(targetX)) {
-            yMetersPerSecond = -strafeController.calculate(targetX, -3.11);
-            yMetersPerSecond = MathUtil.clamp(yMetersPerSecond, -0.5, 0.5);
-        }
-        else {
-            yMetersPerSecond = 0;
+        Pose3d campose = Limelight.get().getCampose();
+        if (campose == null) {
+            return;
         }
 
-        double targetY = Limelight.get().getTargetY();
-        if (!Double.isNaN(targetY)) {
-            xMetersPerSecond = distanceController.calculate(targetY, -19.04);
-            xMetersPerSecond = MathUtil.clamp(xMetersPerSecond, -0.75, 0.75);
-        }
-        else {
-            xMetersPerSecond = 0;
-        }
+        
     }
 
     @Override
     public boolean isFinished() {
-        return strafeController.atSetpoint() && distanceController.atSetpoint();
+        return false;
     }
 
     @Override
     public void end(boolean interrupted) {
-        System.out.println("High cube node end");
 
-        xMetersPerSecond = 0;
-        yMetersPerSecond = 0;
     }
 }
