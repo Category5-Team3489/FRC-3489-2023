497e37904e677682afd17f2dbb2ae093d08d9f29
3489ProgrammingFleet <98435354+3489ProgrammingFleet@users.noreply.github.com>
2023-03-10 09:41:10 -0500

    Remove imports

--- FRC-3489-2023-Comp/src/main/java/frc/robot/RobotContainer.java
+++ FRC-3489-2023-Comp/src/main/java/frc/robot/RobotContainer.java
@@ -11,9 +11,6 @@ import com.revrobotics.CANSparkMax.IdleMode;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
