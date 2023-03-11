0bca9a72eaa4256da87501c72cab3a920d2697a8
3489ProgrammingFleet <98435354+3489ProgrammingFleet@users.noreply.github.com>
2023-03-10 19:55:15 -0500

    Update RobotContainer.java

--- FRC-3489-2023-Comp/src/main/java/frc/robot/RobotContainer.java
+++ FRC-3489-2023-Comp/src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,7 @@ package frc.robot;
 import java.util.ArrayList;
 import java.util.List;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -15,6 +16,7 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 import frc.robot.Constants.AutoConstants;
 import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.DriveToRelativePose;
 import frc.robot.shuffleboard.Cat5ShuffleboardTab;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Cat5Subsystem;
@@ -84,13 +86,15 @@ public class RobotContainer {
     private void configureBindings() {}
 
     public Command getAutonomousCommand() {
-        switch (autoChooser.getSelected()) {
-            case AutoConstants.ConeThenTaxiAuto:
-                return Autos.getConeThenTaxiAuto();
-            case AutoConstants.ConeThenBalanceAuto:
-                return Autos.getConeThenBalanceAuto();
-        }
+        return new DriveToRelativePose(0, 3, 0, 0.6, 0.5, 90);
+
+        // switch (autoChooser.getSelected()) {
+        //     case AutoConstants.ConeThenTaxiAuto:
+        //         return Autos.getConeThenTaxiAuto();
+        //     case AutoConstants.ConeThenBalanceAuto:
+        //         return Autos.getConeThenBalanceAuto();
+        // }
         
-        return Commands.print("No autonomous command selected");
+        // return Commands.print("No autonomous command selected");
     }
 }
