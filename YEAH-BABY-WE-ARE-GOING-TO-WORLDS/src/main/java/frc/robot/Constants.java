fcea11c2696f9078681b3b7b117e0cda9760c39c
3489ProgrammingFleet <98435354+3489ProgrammingFleet@users.noreply.github.com>
2023-04-11 18:27:04 -0400

    Wrist

--- YEAH-BABY-WE-ARE-GOING-TO-WORLDS/src/main/java/frc/robot/Constants.java
+++ YEAH-BABY-WE-ARE-GOING-TO-WORLDS/src/main/java/frc/robot/Constants.java
@@ -3,4 +3,39 @@ package frc.robot;
 public class Constants {
     public static boolean IsDebugShuffleboardEnabled = false;
     public static boolean IsSDSDebugEnabled = false;
+
+    public static class WristConstants {
+        public enum WristState {
+            Min(-13),
+            Max(5),
+            HighCone(-18.05), // -20.05
+            MinAtHigh(-30),
+            DoubleSubstation(-6.29),
+            HighCube(-8.65),
+            Start(0),
+            Pickup(-1),
+            Carry(-13);
+
+            private final double rotations;
+
+            private WristState(double rotations) {
+                this.rotations = rotations;
+            }
+
+            public double getRotations() {
+                return rotations;
+            }
+        }
+
+        public static final double MotorRevolutionsPerRevolution = (100.0 / 1.0) * (2.0 / 1.0);
+        public static final double MotorRevolutionsPerDegree = MotorRevolutionsPerRevolution / 360.0;
+        public static final double DegreesPerMotorRevolution = 1.0 / MotorRevolutionsPerDegree;
+
+        public static final int StallSmartCurrentLimitAmps = 20;
+        public static final double ProportionalGainPercentPerRevolutionOfError = 0.5;
+        public static final double MinOutputPercent = -0.30; // 15
+        public static final double MaxOutputPercent = 0.30; // 15
+
+        public static final int MotorDeviceId = 12;
+    }
 }
--- YEAH-BABY-WE-ARE-GOING-TO-WORLDS/src/main/java/frc/robot/subsystems/Wrist.java
+++ YEAH-BABY-WE-ARE-GOING-TO-WORLDS/src/main/java/frc/robot/subsystems/Wrist.java
@@ -1,5 +1,102 @@
 package frc.robot.subsystems;
 
-public class Wrist {
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax.ControlType;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANSparkMaxLowLevel.PeriodicFrame;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Cat5Input;
+import frc.robot.Robot;
+import frc.robot.RobotContainer;
+import frc.robot.Constants.WristConstants.WristState;
+import frc.robot.data.shuffleboard.Cat5ShuffleboardTab;
+import frc.robot.enums.GridPosition;
+
+import static frc.robot.Constants.WristConstants.*;
+
+public class Wrist extends Cat5Subsystem{
     
+    // Devices
+    private final CANSparkMax motor = new CANSparkMax(MotorDeviceId, MotorType.kBrushless); // Negative up, positive down
+    private final SparkMaxPIDController pidController;
+    private final RelativeEncoder encoder;
+    
+    // Commands
+    private final CommandBase gotoTargetCommand = getGotoTargetCommand();
+
+    // State
+    private final Indicator indicator;
+    private WristState state = WristState.Start;
+    private double rotations = WristState.Start.getRotations();
+
+    private Wrist(RobotContainer robotContainer, Indicator indicator) {
+        super(robotContainer);
+        this.indicator = indicator;
+
+        setDefaultCommand(gotoTargetCommand);
+
+        //#region Devices
+        pidController = motor.getPIDController();
+        encoder = motor.getEncoder();
+
+        motor.restoreFactoryDefaults();
+        motor.setIdleMode(IdleMode.kBrake);
+        motor.enableVoltageCompensation(12.0);
+        motor.setSmartCurrentLimit(StallSmartCurrentLimitAmps);
+        motor.setPeriodicFramePeriod(PeriodicFrame.kStatus0, 50);//20 // TODO Should this be done in other places or with other frames?
+        motor.setPeriodicFramePeriod(PeriodicFrame.kStatus1, 50);
+        motor.setPeriodicFramePeriod(PeriodicFrame.kStatus2, 50);
+        motor.setPeriodicFramePeriod(PeriodicFrame.kStatus3, 50);
+        motor.setPeriodicFramePeriod(PeriodicFrame.kStatus4, 50);
+        pidController.setP(ProportionalGainPercentPerRevolutionOfError);
+        pidController.setOutputRange(MinOutputPercent, MaxOutputPercent);
+        motor.burnFlash(); // Always remember this - burn flash, not motor
+        //#endregion
+    }
+
+    @Override
+    public void periodic() {
+        double correctionPercent = robotContainer.input.getWristCorrectionPercent();
+        rotations -= correctionPercent * 7.5 * Robot.kDefaultPeriod;
+        if (Arm.get().getGridPosition() == GridPosition.High || Arm.get().getGridPosition() == GridPosition.Mid) {
+            rotations = MathUtil.clamp(rotations, WristState.MinAtHigh.getRotations(), WristState.Max.getRotations());
+        }
+        else {
+            rotations = MathUtil.clamp(rotations, WristState.Min.getRotations(), WristState.Max.getRotations());
+        }
+    }
+
+    // //#region Encoder
+    // private double getEncoderAngleDegrees() {
+    //     double rotations = encoder.getPosition();
+    //     return rotations * DegreesPerMotorRevolution;
+    // }
+    // //#endregion
+
+    //#region Commands
+    private CommandBase getGotoTargetCommand() {
+        return run(() -> {
+            pidController.setReference(rotations, ControlType.kPosition, 0);
+        })
+            .withName("Goto Target");
+    }
+    //#endregion
+
+    //#region Public
+    public WristState getState() {
+        return state;
+    }
+    
+    public void setState(WristState state) {
+        this.state = state;
+
+        rotations = state.getRotations();
+    }
+    //#endregion
 }
